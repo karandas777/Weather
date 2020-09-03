389fd0a8a2987df4f9709650041a40aa1c96254c
karandas777 <karandas.21.08.98@gmail.com>
2020-09-03 10:53:27 +0530

    name changed

--- src/component/LogoHolder.js
+++ src/component/LogoHolder.js
@@ -4,8 +4,9 @@ export default class LogoHolder extends Component {
     render() {
         return (
             <div className="p-3 rounded-lg">
-                <div className="h3 text-muted mt-3 mb-0">
-                <img src={require('../assets/logo.gif')} className="w-25 mb-2 mr-3 evening rounded-pill shadow" alt="logo"/>Weather
+                <div className="h1 text-dark mt-3 mb-0">
+                <img src={require('../assets/logo.gif')} className="w-25 mb-2 mr-3 evening rounded-pill shadow" alt="logo"/>
+                Dark<span className="text-evening">Sky</span>
                 </div>
             </div>
         )
